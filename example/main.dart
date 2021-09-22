e8dd9f988634ac952ed872b42470ffa7c243f8e7
Eduardo Nuzzi <eduardomnuzzi@gmail.com>
2021-09-22 11:46:17 -0300

    add example folder

--- example/main.dart
+++ example/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-
-import '../lib/jigsaw_puzzle.dart';
+import 'package:jigsaw_puzzle/jigsaw_puzzle.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     final puzzleKey = GlobalKey<JigsawWidgetState>();
@@ -20,7 +21,6 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.blueGrey.shade100,
           cardColor: Colors.yellow,
           errorColor: Colors.orange,
-          brightness: Brightness.light,
         ),
         textTheme: Typography.englishLike2018,
       ).copyWith(
@@ -30,38 +30,38 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.white,
         body: SafeArea(
           child: Padding(
-            padding: EdgeInsets.all(24),
+            padding: const EdgeInsets.all(24),
             child: Column(
               children: [
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () async {
-                          await puzzleKey.currentState.generate();
-                        },
-                        child: Text("Generate"),
-                      ),
-                      SizedBox(width: 16),
-                      ElevatedButton(
-                        onPressed: () async {
-                          await puzzleKey.currentState.reset();
-                        },
-                        child: Text("Clear"),
-                      ),
-                    ],
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () async {
+                        await puzzleKey.currentState!.generate();
+                      },
+                      child: const Text('Generate'),
+                    ),
+                    const SizedBox(width: 16),
+                    ElevatedButton(
+                      onPressed: () {
+                        puzzleKey.currentState!.reset();
+                      },
+                      child: const Text('Clear'),
+                    ),
+                  ],
                 ),
                 JigsawPuzzle(
                   gridSize: 5,
-                  image: AssetImage("./assets/Jigsaw.jpg"),
+                  image: const AssetImage('assets/Jigsaw.jpg'),
                   onFinished: () {
-                    print("finished!");
+                    // ignore: avoid_print
+                    print('finished!');
                   },
                   puzzleKey: puzzleKey,
                   onBlockSuccess: () {
-                    print("block success!");
+                    // ignore: avoid_print
+                    print('block success!');
                   },
                 )
               ],
